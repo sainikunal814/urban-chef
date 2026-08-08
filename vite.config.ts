71c4b1665b5982bc9ea03613d532a6a0a635df3d
sainikunal814 <sainikunal814@gmail.com>
2026-08-08 12:18:53 +0530

    Add GitHub Actions workflow for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/urban-chef/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
